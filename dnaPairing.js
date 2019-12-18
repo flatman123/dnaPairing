a483790b62bd9a2b66bbdd2ca895c7f3ff35f910
Jeffrey Mcintyre <flatlandpyhon67@hotmail.com>
2019-12-18 12:45:48 -0500

    Create dnaPairing.js

---
+++ dnaPairing.js
@@ -0,0 +1,26 @@
+function findPair(nucleobase){
+	let dnaCombinations = {
+		'T': 'TA',
+		'A': 'AT',
+		'G': 'GC',
+		'C': 'CG'
+	};
+	//Find matching nucleobases 
+	let pairingBase = Object.keys(dnaCombinations)
+								.filter(prop => nucleobase === prop);
+	return dnaCombinations[pairingBase];
+}
+
+
+function pairElement(nucleobase,...pairs){
+	//Base Condition
+	if (nucleobase === ''){
+		return [];
+	}
+	//Core Logic
+	let pairing = findPair(nucleobase[0]);
+	return pairs
+				.concat([pairing.split('')])
+				.concat(pairElement(nucleobase.replace(nucleobase[0],'')));
+};
+console.log(pairElement('ATCGA'));
